d12b17a38f278b2c34db30430da32e00daee9904
Marco Kirchner <kirchnermarco@gmail.com>
2020-05-15 11:29:25 +0200

    fix emoji parsing #2

--- src/main/kotlin/de/bigboot/ggtools/fang/utils/ReactionEmojiExt.kt
+++ src/main/kotlin/de/bigboot/ggtools/fang/utils/ReactionEmojiExt.kt
@@ -3,8 +3,8 @@ package de.bigboot.ggtools.fang.utils
 import discord4j.core.`object`.reaction.ReactionEmoji
 import discord4j.rest.util.Snowflake
 
-private val emojiRegex = Regex("""<(?<animated>a?):(?<name>\w+):(?<id>\d+)>""")
+private val emojiRegex = Regex("""<(a?):(\w+):(\d+)>""")
 fun String.asReaction(): ReactionEmoji = emojiRegex.find(this)?.let {
-    val (animated, id, name) = it.destructured
+    val (animated, name, id) = it.destructured
     ReactionEmoji.custom(Snowflake.of(id), name, animated.isNotBlank())
 } ?: ReactionEmoji.unicode(this)
