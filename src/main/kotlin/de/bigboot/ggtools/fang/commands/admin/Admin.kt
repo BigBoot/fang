f36191e0e14715598bdf826f7b5c2a5036928c8e
BigBoot <kirchnermarco@gmail.com>
2020-06-10 17:10:22 +0200

    add restart command

--- src/main/kotlin/de/bigboot/ggtools/fang/commands/admin/Admin.kt
+++ src/main/kotlin/de/bigboot/ggtools/fang/commands/admin/Admin.kt
@@ -8,5 +8,16 @@ class Admin : CommandGroupSpec("admin", "Admin commands") {
     override val build: CommandGroupBuilder.() -> Unit = {
         group(Server())
         group(Group())
+        
+         command("restart", "restart the bot") {
+            onCall {
+                @Suppress("BlockingMethodInNonBlockingContext")
+                ProcessBuilder("/usr/bin/supervisorctl", "restart", "fang").start()
+
+                channel().createMessage { message ->
+                    message.setContent("I'm sorry for disappointing you")
+                }.awaitSingle()
+            }
+        }
     }
 }
