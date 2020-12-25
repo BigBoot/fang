3a6a056114c4d1c8a62ee238d2dd0b2e8c774fac
Marco Kirchner <kirchnermarco@gmail.com>
2020-12-25 19:41:14 +0100

    update

--- src/main/kotlin/de/bigboot/ggtools/fang/commands/admin/Admin.kt
+++ src/main/kotlin/de/bigboot/ggtools/fang/commands/admin/Admin.kt
@@ -2,10 +2,9 @@ package de.bigboot.ggtools.fang.commands.admin
 
 import de.bigboot.ggtools.fang.CommandGroupBuilder
 import de.bigboot.ggtools.fang.CommandGroupSpec
-import de.bigboot.ggtools.fang.Config
 import de.bigboot.ggtools.fang.commands.admin.group.Group
-import de.bigboot.ggtools.fang.utils.parseArgs
 import kotlinx.coroutines.reactive.awaitSingle
+import kotlin.system.exitProcess
 
 class Admin : CommandGroupSpec("admin", "Admin commands") {
     override val build: CommandGroupBuilder.() -> Unit = {
@@ -14,12 +13,11 @@ class Admin : CommandGroupSpec("admin", "Admin commands") {
         
          command("restart", "restart the bot") {
             onCall {
-                @Suppress("BlockingMethodInNonBlockingContext")
-                ProcessBuilder(parseArgs(Config.bot.restart_command)).start()
-
                 channel().createMessage { message ->
                     message.setContent("I'm sorry for disappointing you")
                 }.awaitSingle()
+
+                exitProcess(0)
             }
         }
     }
